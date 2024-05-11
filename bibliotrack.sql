477c8fb10322cec45378da9acc5fb6c4d84c32fa
unknown <boccadoro.erika@einaudicorreggio.it>
2024-05-11 12:17:18 +0200

    aggiornato db

--- bibliotrack.sql
+++ bibliotrack.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Mag 10, 2024 alle 10:28
+-- Creato il: Mag 10, 2024 alle 10:40
 -- Versione del server: 10.4.28-MariaDB
--- Versione PHP: 8.0.28
+-- Versione PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -35,6 +35,13 @@ CREATE TABLE `feedback_libri` (
   `FK_Id_libro` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dump dei dati per la tabella `feedback_libri`
+--
+
+INSERT INTO `feedback_libri` (`PK_Id_feedback`, `Voto`, `Feedback`, `FK_Id_utente`, `FK_Id_libro`) VALUES
+(5, 6, 'Molto coinvolgente, consigliatissimo', 3, 5);
+
 -- --------------------------------------------------------
 
 --
@@ -46,7 +53,7 @@ CREATE TABLE `libri` (
   `Titolo` varchar(100) NOT NULL,
   `Autore` varchar(50) NOT NULL,
   `Casa_editrice` varchar(50) NOT NULL,
-  `Anno_pubblicazione` int(255) DEFAULT NULL,
+  `Anno_pubblicazione` int(4) DEFAULT NULL,
   `Collana` varchar(50) DEFAULT NULL,
   `Genere` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -57,11 +64,30 @@ CREATE TABLE `libri` (
 
 INSERT INTO `libri` (`PK_Id_libro`, `Titolo`, `Autore`, `Casa_editrice`, `Anno_pubblicazione`, `Collana`, `Genere`) VALUES
 (1, 'Il mastino dei Baskerville', 'Arthur Conan Doyle', 'Feltrinelli', 1902, 'GrandiGialli', 'Giallo'),
-(2, 'Il grande Gatsby', 'Francis Scott Fitzg', 'Feltrinelli', 1925, 'GrandiClassici', 'Tragedia'),
-(3, 'La bibbia', '', 'Feltrinelli', 33, 'Religione', 'Teologia'),
-(6, 'Delitto e castigo', 'Fëdor Dostoevskij ', 'Feltrinelli', 1866, 'Classici Russi', 'Narrativa psicologica'),
-(10, 'Il visconte dimezzato', 'Italo Calvino', 'Mondadori', 1952, 'Libri Fantastici', 'commedia'),
-(11, 'Il cavaliere inesistente', 'Italo Calvino', 'Mondadori', 1959, 'Libri Fantastici', 'commedia');
+(3, 'La bibbia', '', 'Feltrinelli', 0, 'Religione', 'Teologia'),
+(4, 'La bella addormentata nel bosco', 'Gianbattista Basile', 'Giunti', 1922, 'Bambini', 'Fiaba'),
+(5, 'I 10 piccoli indiani', 'Agatha Christie', 'Giunti', 1939, 'Grandi classici', 'Gialli'),
+(6, 'Sei personaggi in cerca di autore', 'Luigi Pirandello', 'Gli Adelphi', 1921, 'Classici italiani', 'Commedia teatrale'),
+(7, 'Il grande Gatsby', 'Francis Scott Fitzgerald', 'Mondadori', 1925, 'I Meridiani', 'Romanzo'),
+(8, '1984', 'George Orwell', 'Mondadori', 1949, 'Oscar Moderni', 'Romanzo distopico'),
+(9, 'Orgoglio e pregiudizio', 'Jane Austen', 'Garzanti', 1813, 'I grandi libri', 'Romanzo'),
+(10, 'Harry Potter e la pietra filosofale', 'Joanne Rowling', 'Salani', 1997, 'Harry Potter', 'Fantasy'),
+(11, 'La ragazza di Bube', 'Carlo Cassola', 'Einaudi', 1960, 'Einaudi tascabili', 'Romanzo'),
+(12, 'To Kill a Mockingbird', 'Harper Lee', 'Mondadori', 1960, 'Oscar Moderni', 'Romanzo'),
+(13, 'Il signore degli anelli', 'John Ronald Reuel Tolkien', 'Bompiani', 1954, 'Tascabili Bompiani', 'Fantasy'),
+(14, 'Piccole donne', 'Louisa May Alcott', 'Mondadori', 1868, 'Oscar Moderni', 'Romanzo'),
+(15, 'Cime tempestose', 'Emily Brontë', 'Mondadori', 184, 'Mondadori', 'Romanzo'),
+(16, 'Cent\'anni di solitudine', 'Gabriel García Márquez', 'Mondadori', 1967, 'I Meridiani', 'Romanzo'),
+(17, 'Guerra e pace', 'Lev Nikolaevič Tolstoj', 'Einaudi', 1869, 'Einaudi tascabili', 'Romanzo storico'),
+(18, 'La colpa è delle stelle', 'John Green', 'Mondadori', 2012, 'Oscar Moderni', 'Romanzo per ragazzi'),
+(19, 'Anna Karenina', 'Lev Nikolaevič Tolstoj', 'Mondadori', 1877, 'I grandi libri', 'Romanzo'),
+(20, 'La storia infinita', 'Michael Ende', 'Mondadori', 1979, 'Oscar Moderni', 'Fantasy'),
+(21, 'Lo Hobbit', 'John Ronald Reuel Tolkien', 'Bompiani', 1937, 'Oscar Draghi', 'Fantasy'),
+(22, 'I miserabili', 'Victor Hugo', 'Mondadori', 1862, 'I grandi libri', 'Romanzo'),
+(23, 'La fattoria degli animali', 'George Orwell', 'Mondadori', 1945, 'Oscar Moderni', 'Romanzo satirico'),
+(24, 'Il codice da Vinci', 'Dan Brown', 'Mondadori', 2003, 'Oscar Moderni', 'Romanzo thriller'),
+(25, 'Harry Potter e il prigioniero di Azkaban', 'Joanne Rowling', 'Salani', 1999, 'Harry Potter', 'Fantasy'),
+(26, 'Le cronache di Narnia: Il leone, la strega e l\'armadio', 'Clive Staples Lewis', 'Mondadori', 1950, 'Oscar Moderni', 'Fantasy');
 
 -- --------------------------------------------------------
 
@@ -74,35 +100,17 @@ CREATE TABLE `prestiti` (
   `FK_Id_utente` int(11) NOT NULL,
   `FK_Id_libro` int(11) NOT NULL,
   `Scadenza_prestito` date NOT NULL,
-  `inizio_prestito` date DEFAULT NULL
+  `Inizio_prestito` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dump dei dati per la tabella `prestiti`
 --
 
-INSERT INTO `prestiti` (`PK_Id_prestito`, `FK_Id_utente`, `FK_Id_libro`, `Scadenza_prestito`, `inizio_prestito`) VALUES
-(15, 1, 2, '2024-08-08', '2024-05-08'),
-(17, 1, 3, '2024-07-08', '2024-05-08'),
-(18, 1, 10, '2024-07-10', '2024-05-10');
-
--- --------------------------------------------------------
-
---
--- Struttura della tabella `superusers`
---
-
-CREATE TABLE `superusers` (
-  `password_superuser` varchar(250) DEFAULT NULL,
-  `username` varchar(30) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dump dei dati per la tabella `superusers`
---
-
-INSERT INTO `superusers` (`password_superuser`, `username`) VALUES
-('$2y$10$uRZe2ti6QoBUoY0Ekc1CJOGYUxMzyH56SzYqdimul0wUH9HQVSNcC', 'admin');
+INSERT INTO `prestiti` (`PK_Id_prestito`, `FK_Id_utente`, `FK_Id_libro`, `Scadenza_prestito`, `Inizio_prestito`) VALUES
+(4, 4, 6, '2024-04-24', '2024-05-24'),
+(5, 3, 4, '2024-04-24', '2024-05-24'),
+(6, 3, 1, '2024-04-23', '2024-05-23');
 
 -- --------------------------------------------------------
 
@@ -122,12 +130,11 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`username`, `password`, `email`) VALUES
 ('admin', '$2y$10$9/Psjkv2jSwRS61rziHSXuLRik/7uMOZprWTGtjRMp73xJ9FzgnIW', 'admin@gmail.com'),
-('angela', '$2y$10$4cn419TLs0O5aBP/PQ3I9OQXsxNvXkn0w.tKSYO6K48JTuuqW2j3a', 'angela.tirabassi2005@libero.it'),
 ('angelatirabassi', '$2y$10$u0YWKFV54kUrDTyrQymyT.hA4IbLI1GmFP5zHUTD0HT.x6oZUt6kC', 'angelatirabassi05@gmail.com'),
+('f', '$2y$10$70RZSR77qH/Mr5ugsYElf.GFsq9RU0kRJzWVHg58ZGqNUFJiqKbaG', 'f@gmail.com'),
+('martinob', '$2y$10$g0OuvnS.RoKydFxUdNBFNun22cCC9hVGq5QIwWU6PT7WtP29i6Jhq', 'martin.boa@gmail.com'),
 ('MAtti04', '$2y$10$08F7sdGe8zoz/ROOAZMo6e/wmB.HywPyjXmuTUWjliUZ5zZd46rdi', 'scarpam204@gmail.com'),
-('ometto', '$2y$10$byj31OEjhOw6ZgAwqDAM1.xYc/RpRDRsK3pVE1j/PKP9hKZs.2ZYC', 'iacopo.ferrari@einaudicorreggi'),
-('roooooot', '$2y$10$Npqs3oOQlKMHtWe8hYUnkOuRxOUX0Jx2a5P/b./jRTXdaN8jcF2Vq', 'root@gmail.com'),
-('sotto', '$2y$10$kcJ0aRN7zqmJfmQ9c30l5ezdwTz9iKwBowozZ4Nr1uuPrxlVCxRLC', 'sotto@gmail.com');
+('ometto', '$2y$10$byj31OEjhOw6ZgAwqDAM1.xYc/RpRDRsK3pVE1j/PKP9hKZs.2ZYC', 'iacopo.ferrari@einaudicorreggi');
 
 -- --------------------------------------------------------
 
@@ -139,19 +146,18 @@ CREATE TABLE `utenti` (
   `PK_Id_utente` int(11) NOT NULL,
   `Nome` varchar(50) NOT NULL,
   `Cognome` varchar(50) NOT NULL,
+  `Mail` varchar(100) DEFAULT NULL,
   `Telefono` varchar(10) DEFAULT NULL,
-  `fk_user` varchar(11) DEFAULT NULL,
-  `codice_fiscale` varchar(200) DEFAULT NULL,
-  `indirizzo` varchar(200) DEFAULT NULL
+  `FK_User` varchar(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dump dei dati per la tabella `utenti`
 --
 
-INSERT INTO `utenti` (`PK_Id_utente`, `Nome`, `Cognome`, `Telefono`, `fk_user`, `codice_fiscale`, `indirizzo`) VALUES
-(1, 'Riccardo', 'Ruozzi', '3389813841', 'admin', NULL, NULL),
-(6, 'sotto', 'sottoni', NULL, 'sotto', 'sssssssssss', 'wwwww');
+INSERT INTO `utenti` (`PK_Id_utente`, `Nome`, `Cognome`, `Mail`, `Telefono`, `FK_User`) VALUES
+(3, 'Riccardo', 'Ruozzi', 'ruozziriccardo4@gmail.com', '3389813841', 'admin'),
+(4, 'Erika', 'Boccadoro', 'erika.bocca@gmail.com', '3667425670', 'admin');
 
 --
 -- Indici per le tabelle scaricate
@@ -180,12 +186,6 @@ ALTER TABLE `prestiti`
   ADD KEY `FK_Id_libro` (`FK_Id_libro`);
 
 --
--- Indici per le tabelle `superusers`
---
-ALTER TABLE `superusers`
-  ADD KEY `fk_superuser` (`username`);
-
---
 -- Indici per le tabelle `users`
 --
 ALTER TABLE `users`
@@ -196,23 +196,29 @@ ALTER TABLE `users`
 --
 ALTER TABLE `utenti`
   ADD PRIMARY KEY (`PK_Id_utente`),
-  ADD KEY `fk_su_username` (`fk_user`);
+  ADD KEY `Fk_Utenti` (`FK_User`);
 
 --
 -- AUTO_INCREMENT per le tabelle scaricate
 --
 
 --
+-- AUTO_INCREMENT per la tabella `feedback_libri`
+--
+ALTER TABLE `feedback_libri`
+  MODIFY `PK_Id_feedback` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT per la tabella `libri`
 --
 ALTER TABLE `libri`
-  MODIFY `PK_Id_libro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `PK_Id_libro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT per la tabella `prestiti`
 --
 ALTER TABLE `prestiti`
-  MODIFY `PK_Id_prestito` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `PK_Id_prestito` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT per la tabella `utenti`
@@ -239,16 +245,10 @@ ALTER TABLE `prestiti`
   ADD CONSTRAINT `prestiti_ibfk_2` FOREIGN KEY (`FK_Id_libro`) REFERENCES `libri` (`PK_Id_libro`);
 
 --
--- Limiti per la tabella `superusers`
---
-ALTER TABLE `superusers`
-  ADD CONSTRAINT `fk_superuser` FOREIGN KEY (`username`) REFERENCES `users` (`username`);
-
---
 -- Limiti per la tabella `utenti`
 --
 ALTER TABLE `utenti`
-  ADD CONSTRAINT `fk_su_username` FOREIGN KEY (`fk_user`) REFERENCES `users` (`username`);
+  ADD CONSTRAINT `Fk_Utenti` FOREIGN KEY (`FK_User`) REFERENCES `users` (`username`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
