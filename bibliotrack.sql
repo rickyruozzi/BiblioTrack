1cefc055627ea9a2e05a444980ee49a9001b8b22
rickyruozzi <ruozziriccardo4@gmail.com>
2024-05-12 15:29:06 +0200

    piccoli aggiornamenti

--- bibliotrack.sql
+++ bibliotrack.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Mag 12, 2024 alle 14:49
+-- Creato il: Mag 12, 2024 alle 15:28
 -- Versione del server: 10.4.28-MariaDB
 -- Versione PHP: 8.0.28
 
@@ -20,20 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `bibliotrack`
 --
-
--- --------------------------------------------------------
-
---
--- Struttura della tabella `feedback_libri`
---
-
-CREATE TABLE `feedback_libri` (
-  `id_feedback` int(11) NOT NULL,
-  `id_libro` int(11) NOT NULL,
-  `username` varchar(30) NOT NULL,
-  `voto` int(11) NOT NULL CHECK (`voto` <= 10 and `voto` >= 1),
-  `feedback` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -69,7 +55,7 @@ INSERT INTO `libri` (`PK_Id_libro`, `Titolo`, `Autore`, `Casa_editrice`, `Anno_p
 (12, 'To Kill a Mockingbird', 'Harper Lee', 'Mondadori', 1960, 'Oscar Moderni', 'Romanzo'),
 (13, 'Il signore degli anelli', 'John Ronald Reuel Tolkien', 'Bompiani', 1954, 'Tascabili Bompiani', 'Fantasy'),
 (14, 'Piccole donne', 'Louisa May Alcott', 'Mondadori', 1868, 'Oscar Moderni', 'Romanzo'),
-(15, 'Cime tempestose', 'Emily Brontë', 'Mondadori', 184, 'Mondadori', 'Romanzo'),
+(15, 'Cime tempestose', 'Emily Brontë', 'Mondadori', 1846, 'Mondadori', 'Romanzo'),
 (16, 'Cent\'anni di solitudine', 'Gabriel García Márquez', 'Mondadori', 1967, 'I Meridiani', 'Romanzo'),
 (17, 'Guerra e pace', 'Lev Nikolaevič Tolstoj', 'Einaudi', 1869, 'Einaudi tascabili', 'Romanzo storico'),
 (18, 'La colpa è delle stelle', 'John Green', 'Mondadori', 2012, 'Oscar Moderni', 'Romanzo per ragazzi'),
@@ -82,91 +68,9 @@ INSERT INTO `libri` (`PK_Id_libro`, `Titolo`, `Autore`, `Casa_editrice`, `Anno_p
 (25, 'Harry Potter e il prigioniero di Azkaban', 'Joanne Rowling', 'Salani', 1999, 'Harry Potter', 'Fantasy'),
 (26, 'Le cronache di Narnia: Il leone, la strega e l\'armadio', 'Clive Staples Lewis', 'Mondadori', 1950, 'Oscar Moderni', 'Fantasy');
 
--- --------------------------------------------------------
-
---
--- Struttura della tabella `prestiti`
---
-
-CREATE TABLE `prestiti` (
-  `PK_Id_prestito` int(11) NOT NULL,
-  `FK_Id_utente` int(11) NOT NULL,
-  `FK_Id_libro` int(11) NOT NULL,
-  `Scadenza_prestito` date NOT NULL,
-  `Inizio_prestito` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dump dei dati per la tabella `prestiti`
---
-
-INSERT INTO `prestiti` (`PK_Id_prestito`, `FK_Id_utente`, `FK_Id_libro`, `Scadenza_prestito`, `Inizio_prestito`) VALUES
-(4, 4, 6, '2024-04-24', '2024-05-24'),
-(5, 3, 4, '2024-04-24', '2024-05-24'),
-(6, 3, 1, '2024-04-23', '2024-05-23'),
-(7, 3, 11, '2024-07-11', '2024-05-11'),
-(8, 3, 11, '2024-07-11', '2024-05-11'),
-(9, 3, 11, '2024-07-11', '2024-05-11'),
-(11, 3, 11, '2024-07-11', '2024-05-11');
-
--- --------------------------------------------------------
-
---
--- Struttura della tabella `users`
---
-
-CREATE TABLE `users` (
-  `username` varchar(30) NOT NULL,
-  `password` varchar(250) NOT NULL,
-  `email` varchar(30) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dump dei dati per la tabella `users`
---
-
-INSERT INTO `users` (`username`, `password`, `email`) VALUES
-('admin', '$2y$10$9/Psjkv2jSwRS61rziHSXuLRik/7uMOZprWTGtjRMp73xJ9FzgnIW', 'admin@gmail.com'),
-('angelatirabassi', '$2y$10$u0YWKFV54kUrDTyrQymyT.hA4IbLI1GmFP5zHUTD0HT.x6oZUt6kC', 'angelatirabassi05@gmail.com'),
-('f', '$2y$10$70RZSR77qH/Mr5ugsYElf.GFsq9RU0kRJzWVHg58ZGqNUFJiqKbaG', 'f@gmail.com'),
-('martinob', '$2y$10$g0OuvnS.RoKydFxUdNBFNun22cCC9hVGq5QIwWU6PT7WtP29i6Jhq', 'martin.boa@gmail.com'),
-('MAtti04', '$2y$10$08F7sdGe8zoz/ROOAZMo6e/wmB.HywPyjXmuTUWjliUZ5zZd46rdi', 'scarpam204@gmail.com'),
-('ometto', '$2y$10$byj31OEjhOw6ZgAwqDAM1.xYc/RpRDRsK3pVE1j/PKP9hKZs.2ZYC', 'iacopo.ferrari@einaudicorreggi');
-
--- --------------------------------------------------------
-
---
--- Struttura della tabella `utenti`
---
-
-CREATE TABLE `utenti` (
-  `PK_Id_utente` int(11) NOT NULL,
-  `Nome` varchar(50) NOT NULL,
-  `Cognome` varchar(50) NOT NULL,
-  `Mail` varchar(100) DEFAULT NULL,
-  `Telefono` varchar(10) DEFAULT NULL,
-  `FK_User` varchar(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dump dei dati per la tabella `utenti`
---
-
-INSERT INTO `utenti` (`PK_Id_utente`, `Nome`, `Cognome`, `Mail`, `Telefono`, `FK_User`) VALUES
-(3, 'Riccardo', 'Ruozzi', 'ruozziriccardo4@gmail.com', '3389813841', 'admin'),
-(4, 'Erika', 'Boccadoro', 'erika.bocca@gmail.com', '3667425670', 'admin');
-
 --
 -- Indici per le tabelle scaricate
 --
-
---
--- Indici per le tabelle `feedback_libri`
---
-ALTER TABLE `feedback_libri`
-  ADD PRIMARY KEY (`id_feedback`),
-  ADD KEY `id_libro` (`id_libro`),
-  ADD KEY `username` (`username`);
 
 --
 -- Indici per le tabelle `libri`
@@ -175,77 +79,14 @@ ALTER TABLE `libri`
   ADD PRIMARY KEY (`PK_Id_libro`);
 
 --
--- Indici per le tabelle `prestiti`
---
-ALTER TABLE `prestiti`
-  ADD PRIMARY KEY (`PK_Id_prestito`),
-  ADD KEY `FK_Id_utente` (`FK_Id_utente`),
-  ADD KEY `FK_Id_libro` (`FK_Id_libro`);
-
---
--- Indici per le tabelle `users`
---
-ALTER TABLE `users`
-  ADD UNIQUE KEY `username` (`username`);
-
---
--- Indici per le tabelle `utenti`
---
-ALTER TABLE `utenti`
-  ADD PRIMARY KEY (`PK_Id_utente`),
-  ADD KEY `Fk_Utenti` (`FK_User`);
-
---
 -- AUTO_INCREMENT per le tabelle scaricate
 --
-
---
--- AUTO_INCREMENT per la tabella `feedback_libri`
---
-ALTER TABLE `feedback_libri`
-  MODIFY `id_feedback` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT per la tabella `libri`
 --
 ALTER TABLE `libri`
   MODIFY `PK_Id_libro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
-
---
--- AUTO_INCREMENT per la tabella `prestiti`
---
-ALTER TABLE `prestiti`
-  MODIFY `PK_Id_prestito` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
-
---
--- AUTO_INCREMENT per la tabella `utenti`
---
-ALTER TABLE `utenti`
-  MODIFY `PK_Id_utente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- Limiti per le tabelle scaricate
---
-
---
--- Limiti per la tabella `feedback_libri`
---
-ALTER TABLE `feedback_libri`
-  ADD CONSTRAINT `feedback_libri_ibfk_1` FOREIGN KEY (`id_libro`) REFERENCES `libri` (`PK_Id_libro`),
-  ADD CONSTRAINT `feedback_libri_ibfk_2` FOREIGN KEY (`username`) REFERENCES `users` (`username`);
-
---
--- Limiti per la tabella `prestiti`
---
-ALTER TABLE `prestiti`
-  ADD CONSTRAINT `prestiti_ibfk_1` FOREIGN KEY (`FK_Id_utente`) REFERENCES `utenti` (`PK_Id_utente`),
-  ADD CONSTRAINT `prestiti_ibfk_2` FOREIGN KEY (`FK_Id_libro`) REFERENCES `libri` (`PK_Id_libro`);
-
---
--- Limiti per la tabella `utenti`
---
-ALTER TABLE `utenti`
-  ADD CONSTRAINT `Fk_Utenti` FOREIGN KEY (`FK_User`) REFERENCES `users` (`username`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
